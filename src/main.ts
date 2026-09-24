be1471ecf9202331690e5ffee9ee8654b46c90d9
Hastingamo <hezekiahhastings14@gmail.com>
2026-09-24 20:20:28 +0100

    the

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://cardealership-wine.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://cardealership-wine.vercel.app'],
     credentials: true,
   });
 
